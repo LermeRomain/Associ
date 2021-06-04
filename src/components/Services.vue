0939692b4c4bede359f9fb2af6bbf30af0c28279
LermeRomain <romainlerme@hotmail.fr>
2021-06-04 14:05:59 +0200

    Initial commit

---
+++ src/components/Services.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="services container-fluid" id="services">
+<div class="container">
+        <div class="row container">
+          <div class="icon col-md-4 text-center">
+          </div>
+
+          <div class="icon col-md-4 text-center">
+          </div>
+
+          <div class="icon col-md-4 text-center">
+          </div>
+        </div>
+        </div>
+	</div>
+
+</template>
+
+
+<script>
+
+export default {
+    name:'Services'
+}
+
+</script>
+
+
+<style scoped>
+
+
+
+@media screen and (max-width: 1300px){
+  .icon {
+    padding-top: 15px;
+    padding-bottom: 15px;
+  }
+  img {width: 30%}
+}
+
+
+.service .container {
+  background-color: #444444;
+}
+
+#services {
+  padding-top: 70px;
+  padding-bottom: 70px;
+  background-color: #f2f2f2;
+
+
+}
+
+h4 {
+  margin: 15px;
+  font-weight: bold;
+}
+
+p {
+  font-size: 12px;
+  font-weight: lighter;
+}
+
+</style>
