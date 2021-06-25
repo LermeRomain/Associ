34deec31eb886d7d61b9e205691d4d521234d035
LermeRomain <romainlerme@hotmail.fr>
2021-06-25 11:27:25 +0200

    mailing

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -28,14 +28,84 @@
           </div>
         </div>
 
-        <div class="col-md-6 text-left block_2">
-          <h1>Contactez-nous</h1>
-          <div class="menub">
-            <input type="email" name="EMAIL" id="email" placeholder="Entrer votre email">
-            <p1><input type="checkbox">  Associ vous informe, que l'on collectera les données personnelles dans le cadre réglementé du RGPD. Pour
-              plus d’informations, nous vous invitons à vous rendre sur notre page de <span>Politique de confidentialité.</span>
-            </p1>
-            <input type="submit" class="btn2 btn-primary" value="Validé">
+        <div class="col-md-6 text-center block_2">
+          <div id="sib-form-container" class="sib-form-container">
+            <div id="sib-container" class="sib-container--large sib-container--vertical"
+                 style="text-align:center; background-color:#F0F1FE; max-width:540px; padding: 0px">
+              <form id="sib-form" method="POST"
+                    action="https://b01bfbbf.sibforms.com/serve/MUIEAKN9rd2eqo9z8cE8vnUFwc-fpg7mMyoFuszcFcJVsnIGURcDTxXQH78RzO02CBWFnwKYyixykhTR7Ag5-MAeW6y5OqCIIkB9ODPJMoAZx3c_HOtmDlVqsdKrNcHDPM16-dt8MRtpT_C4_IBu1Y8d4VZz3qc0mzoyI3OEc4fU6Sf40OBW_Y7mC_pA0LxCbjXB4rE3mtCCFE5x">
+                <div>
+                  <div class="sib-form-block">
+                    <h1>Je m’abonne à la newsletter</h1>
+                  </div>
+                </div>
+                <div>
+                  <div class="sib-input sib-form-block">
+                    <div class="form__entry entry_block">
+                      <div class="form__label-row ">
+                        <div class="entry__field" style="  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25)); padding: 6px;">
+                          <input class="input" type="text" id="EMAIL" name="EMAIL" autocomplete="off" placeholder="EMAIL"
+                                 data-required="true" required/>
+                        </div>
+                      </div>
+
+                      <label class="entry__error entry__error--primary"
+                             style="font-size:16px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#661d1d; background-color:#ffeded; border-radius:3px; border-color:#ff4949;">
+                      </label>
+                      <label class="entry__specification"
+                             style="font-size:12px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#8390A4;">
+                        Veuillez renseigner votre adresse email pour vous inscrire. Ex. : abc@xyz.com
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <div class="sib-optin sib-form-block" data-required="true">
+                    <div class="form__entry entry_mcq">
+                      <div class="form__label-row ">
+                        <div class="entry__choice">
+                          <label>
+                            <input type="checkbox" class="input_replaced" value="1" id="OPT_IN" name="OPT_IN" required/>
+                            <span class="checkbox checkbox_tick_positive"></span><span
+                              style="font-size:14px; text-align:left; font-family:&quot;Helvetica&quot;, sans-serif; color:#3C4858; background-color:transparent;"><p>Associ vous informe, que l'on collectera les données personnelles dans le cadre réglementé du RGPD. Pour plus d’informations, nous vous invitons à vous rendre sur notre page de Politique de confidentialité.</p><span
+                              data-required="*" style="display: inline;"
+                              class="entry__label entry__label_optin"></span></span>
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                </div>
+                <div>
+                  <div class="sib-form-block" style="text-align: left">
+                    <button class="sib-form-block__button sib-form-block__button-with-loader"
+                            style=" background: #404DEF;
+                              border-radius: 40px;
+                              font-family: Roboto;
+                              font-style: normal;
+                              font-weight: bold;
+                              font-size: 20px;
+                              line-height: 100.8%;
+                              border-radius: 40px;
+                              text-align: center;
+                              color: #FFFFFF;"
+                            form="sib-form" type="submit">
+                      <svg class="icon clickable__icon progress-indicator__icon sib-hide-loader-icon" viewBox="0 0 512 512">
+                        <path
+                            d="M460.116 373.846l-20.823-12.022c-5.541-3.199-7.54-10.159-4.663-15.874 30.137-59.886 28.343-131.652-5.386-189.946-33.641-58.394-94.896-95.833-161.827-99.676C261.028 55.961 256 50.751 256 44.352V20.309c0-6.904 5.808-12.337 12.703-11.982 83.556 4.306 160.163 50.864 202.11 123.677 42.063 72.696 44.079 162.316 6.031 236.832-3.14 6.148-10.75 8.461-16.728 5.01z"
+                        />
+                      </svg>
+                      S'inscrire
+                    </button>
+                  </div>
+                </div>
+                <input type="text" name="email_address_check" value="" class="input--hidden">
+                <input type="hidden" name="locale" value="fr">
+                <input type="hidden" name="html_type" value="simple">
+              </form>
+            </div>
           </div>
         </div>
       </div>
@@ -85,14 +155,6 @@ p1 {
 
 }
 
-a:hover {
-  transform: scale(1.1);
-}
-
-.block_2 {
-  padding-bottom: 50px;
-}
-
 h1 {
   font-family: Roboto;
   font-style: normal;
@@ -132,39 +194,56 @@ h1 {
 }
 
 
-.btn2 {
-  background: #404DEF;
-  border-radius: 40px;
+span {
+  color: #404DEF;
   font-family: Roboto;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 20px;
-  line-height: 100.8%;
-  width: 25%;
-  height: 50px;
-  border-radius: 40px;
-  text-align: center;
-
-  color: #FFFFFF;
 }
 
-.btn2:hover{
+
+
+
+
+
+
+-----------
+@font-face {
+  font-display: block;
+  font-family: Roboto;
+  src: url(https://assets.sendinblue.com/font/Roboto/Latin/normal/normal/7529907e9eaf8ebb5220c5f9850e3811.woff2) format("woff2"), url(https://assets.sendinblue.com/font/Roboto/Latin/normal/normal/25c678feafdc175a70922a116c9be3e7.woff) format("woff")
+}
+
+@font-face {
+  font-display: fallback;
+  font-family: Roboto;
+  font-weight: 600;
+  src: url(https://assets.sendinblue.com/font/Roboto/Latin/medium/normal/6e9caeeafb1f3491be3e32744bc30440.woff2) format("woff2"), url(https://assets.sendinblue.com/font/Roboto/Latin/medium/normal/71501f0d8d5aa95960f6475d5487d4c2.woff) format("woff")
+}
+
+@font-face {
+  font-display: fallback;
+  font-family: Roboto;
+  font-weight: 700;
+  src: url(https://assets.sendinblue.com/font/Roboto/Latin/bold/normal/3ef7cf158f310cf752d5ad08cd0e7e60.woff2) format("woff2"), url(https://assets.sendinblue.com/font/Roboto/Latin/bold/normal/ece3a1d82f18b60bcce0211725c476aa.woff) format("woff")
+}
+
+#sib-container input:-ms-input-placeholder {
+  text-align: left;
+  font-family: "Roboto";
+}
+
+#sib-container input::placeholder {
+  text-align: left;
+  font-family: "Roboto";
+  color: #c0ccda;
+}
+
+.sib-form-block__button-with-loader:hover{
   text-decoration-line: none;
   background: #FFFFFF;
   border: 2px solid #404DEF;
   color: #404DEF;
   box-sizing: border-box;
   border-radius: 50px;
-}
-
-span {
-  color: #404DEF;
-  font-family: Roboto;
-}
-
-#email{
-  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-  padding: 6px;
 }
 
 </style>
