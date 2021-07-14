f1a0029180cbbebc5720d9e4bc6552f3dd566255
LermeRomain <romainlerme@hotmail.fr>
2021-07-14 20:49:26 +0200

    communique

--- src/components/Communiquer.vue
+++ src/components/Communiquer.vue
@@ -14,7 +14,8 @@
           </p>
         </div>
         <div class="col-md-5">
-          <img src="../assets/Conference.svg" alt="logo pour les conference" class="d-none d-md-block" style="width: 100%; margin-top: 100px">
+          <img src="../assets/Conference.svg" alt="logo pour les conference" class="d-none d-md-block"
+               style="width: 100%; margin-top: 100px">
         </div>
       </div>
     </div>
@@ -23,25 +24,31 @@
         Communiqué de presse
       </h2>
       <div class="row offset-md-1">
-        <img class="col-md-7" src="../assets/Pitch.svg" alt="logo pour les pitch" style="width: 100%">
+        <img class="col-md-6" src="../assets/Pitch.svg" alt="logo pour les pitch" style="width: 100%">
+        <div class="col-md-4 offset-md-1">
+          <img src="../assets/Communiqué.png" alt="communiqué de presse"
+               style="width: 100%; border-radius: 25px;box-sizing: border-box; margin-top: 20px">
+          <a href="https://drive.google.com/file/d/15lxPwrA1zknk4IBIZfrECr2qjQuREOKv/view?usp=sharing" target="_blank"><button class="btn1 offset-5">Télécharger</button></a>
+        </div>
       </div>
     </div>
     <h2 class="offset-md-1">
       Dossier de presse
     </h2>
-    <div class="container-fluid background2">
+    <div class="container-fluid background2" style="margin-top: 15% ">
       <div class="row">
-        <div class="col-md-5">
-          <img src="../assets/Conference.svg" alt="logo pour les conference" class="d-none d-md-block" style="width: 100%; margin-top: 100px">
+        <div class="col-md-5 offset-md-1">
+          <img src="../assets/presse.png" alt="dossier de presse" class="d-none d-md-block"
+               style="width: 378px;height: auto; margin-top: 100px; border-radius: 25px">
         </div>
-        <div class="col-md-6 offset-md-1" style="margin-top: 15%">
+        <div class="col-md-6" style="margin-top: 20%">
           <p>
             Apprenez en plus sur Associ, son univers, ses ambitions et surtout ses valeurs.<br><br>
 
             Vous trouverez ici toutes ces informations essentielles à propos de notre entreprise.
           </p>
           <div>
-            <btn>Télécharger</btn>
+            <a href="https://drive.google.com/file/d/1DrNXgr_dFG2Zyst4yO6y02TWTv6-Jck3/view?usp=sharing" target="_blank"><btn>Télécharger</btn></a>
           </div>
         </div>
       </div>
@@ -57,15 +64,13 @@ export default {
 
 <style scoped>
 
-.font{
+.font {
   background-color: #F9F9F9;
-}
-.container-fluid {
-  height: 768px;
 }
 
 .background {
   background-image: url("../assets/Backrose.svg");
+  height: 768px;
 }
 
 .background2 {
@@ -82,8 +87,8 @@ h1 {
   margin-top: 100px;
 }
 
-@media screen and (max-width: 768px){
-  h1{
+@media screen and (max-width: 768px) {
+  h1 {
     font-size: 45px;
   }
 }
@@ -117,13 +122,32 @@ btn {
   border: 2px solid #FFFFFF;
 }
 
-btn:hover{
+btn:hover {
   border-color: #FFFFFF;
   color: #FFFFFF;
   background: none;
 }
 
-a{
-  text-decoration:none;
+.btn1 {
+  font-family: Roboto;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 16px 24px;
+  background: #400186;
+  border-radius: 50px;
+  color: #FFFFFF;
+  border: 2px solid #400186;
+
+  margin-top: 15px;
+}
+
+.btn1:hover {
+  border-color: #400186;
+  color: #400186;
+  background: none;
+}
+
+a {
+  text-decoration: none;
 }
 </style>
